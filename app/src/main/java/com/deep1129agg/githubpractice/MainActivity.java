fd885407143a3b866435588b78a28b2d316336fa
Deepanshu <deep1129agg@gmail.com>
2019-02-21 02:08:55 +0530

    commit

--- app/src/main/java/com/deep1129agg/githubpractice/MainActivity.java
+++ app/src/main/java/com/deep1129agg/githubpractice/MainActivity.java
@@ -3,13 +3,22 @@ package com.deep1129agg.githubpractice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView1, textView2, textView3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView1 = findViewById(R.id.textView1);
+        textView2 = findViewById(R.id.textView2);
+        textView3 = findViewById(R.id.textView3);
+
+
 
         Log.i("msg","for commit");
         Log.i("msg","commit for first branch");
